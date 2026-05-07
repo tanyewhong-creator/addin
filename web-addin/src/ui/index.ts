1dafad54e92677cfc71d6b0a0ab15f4621220da0
tanyewhong-creator <237136388+tanyewhong-creator@users.noreply.github.com>
2026-05-07 15:06:36 +0000

    Phase 1b: barrel export from web-addin/src/ui

---
+++ web-addin/src/ui/index.ts
@@ -0,0 +1,33 @@
+// Lib
+export * from "./lib/cn";
+
+// Icons
+export * as icons from "./icons/allowlist";
+export { Icon } from "./icons/Icon";
+
+// Primitives
+export * from "./primitives/Button";
+export * from "./primitives/Input";
+export * from "./primitives/Textarea";
+export * from "./primitives/Field";
+export * from "./primitives/Card";
+export * from "./primitives/Spinner";
+
+// Layout
+export * from "./layout/Stack";
+export * from "./layout/Cluster";
+export * from "./layout/Container";
+
+// Typography
+export * from "./typography/Heading";
+export * from "./typography/Text";
+export * from "./typography/Caption";
+export * from "./typography/Label";
+
+// Composites
+export * from "./composites/TopBar";
+export * from "./composites/PageShell";
+export * from "./composites/PageHeader";
+export * from "./composites/MessageRow";
+export * from "./composites/EmptyState";
+export * from "./composites/CommandBar";
